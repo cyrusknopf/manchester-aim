54090a456aa611b60b7a778a0e4286d930d8759a
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2022-09-30 17:05:54 +0100

    Program E working as intended

---
+++ programe.py
@@ -0,0 +1,4 @@
+h=int(input("Please enter the height of the cylinder "))
+r=int(input("Please enther the radius of the cylinder "))
+sa=2*3.14159*r*h+2*3.14159*r*r
+print("The surface area of the cylinder is " + str(sa))
