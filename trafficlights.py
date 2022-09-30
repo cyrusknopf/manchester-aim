688158fb4666b9f61f11c0c64471507c2796a2f1
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2022-09-30 15:33:01 +0100

    Traffic light program

---
+++ trafficlights.py
@@ -0,0 +1,9 @@
+def printState():
+	print("Red Light is " + redLight)
+	print("Yellow Light is "+ yellowLight)
+	print("Green Light is "+ greenLight)
+
+redLight = "on"
+yellowLight = "off"
+greenLight = "off"
+printState()
