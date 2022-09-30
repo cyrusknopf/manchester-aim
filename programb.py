2033601dc65594acf47df11b2896955e57471ea0
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2022-09-30 16:24:31 +0100

    Program B working as intended

---
+++ programb.py
@@ -0,0 +1,5 @@
+cel=int(input("Enter temperature in degrees Celsius: "))
+fah=cel*1.8
+print(fah)
+fah=fah+32
+print("The Farenheit equivalent of " + str(cel) + " degrees Celsius is " + str(fah) + " degrees Farenheit.")
