b36dba968999b207c0fa19a47679f32c8161ceae
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2022-09-30 16:18:41 +0100

    Program A, working as intended

---
+++ programa.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+sum = num1+num2
+product = num1*num2
+ratio = num1/num2
+mod = num1%num2
+e = num1**num2
+print(sum)
+print(product)
+print(ratio)
+print(mod)
+print(e)
