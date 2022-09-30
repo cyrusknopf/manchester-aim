5354f9254dc0792a27296a295f326cc33dc5797f
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2022-09-30 15:50:36 +0100

    Boolean variables instead of integers

--- trafficlights.py
+++ trafficlights.py
@@ -3,10 +3,11 @@ def printState():
 	print("Yellow Light is "+ str(yellowLight))
 	print("Green Light is "+ str(greenLight))
 
-redLight = 1
-yellowLight = 0
-greenLight = 0
+redLight = True
+yellowLight = False
+greenLight = False
 printState()
 
 print(3+5)
 print("3"+"5")
+
