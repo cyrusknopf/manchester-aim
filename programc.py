d5354c5e4ac311d40c5172ed61f23fd8b6305d28
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2022-09-30 16:30:34 +0100

    Program C working as intended

---
+++ programc.py
@@ -0,0 +1,5 @@
+r=int(input("Enter the radius of the circle "))
+area=3.14159265359*r*r
+circum=3.14159265359*2*r
+print("The area of the circle is " + str(area))
+print("The circumference of the circle is " +str(circum))
