634fdad440db643621d9ca9f1e7a4b8a66518242
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2022-10-13 22:06:50 +0100

    Working 4x4 sudoku solver

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 import math
+
 grid = [[0,0,0,1],
 		[0,2,0,0],
 		[0,0,4,0],
@@ -7,40 +8,189 @@ size = len(grid)
 area = int(math.sqrt(size))
 for row in grid:
 	print(row)
-solved = False
-while solved == False:
-	target=int(input("Please enter target: "))
-	print('Looking for', target)
-	
-	print("Searching area ",area,"x",area)	#First Quadrant
-	for r in range(area):
-		for c in range(area):
-			if (grid[r][c] == target):
-				print('Target is already in area')
-			else:
-				print(' - Not here')
-
-	print("Searching area ",area,"x",area)	#Second Quadrant
-	for r in range(area):
-		for c in range(area,2*area):
-			if (grid[r][c] == target):
-				print('Target is already in area')
-			else:
-				print(' - Not here')
+targetInQuadrant=False
 
 
-	print("Searching area ",area,"x",area)	#Third Quadrant
-	for r in range(area,2*area):
-		for c in range(area):
-			if (grid[r][c] == target):
-				print('Target is already in area')
-			else:
-				print(' - Not here')
 
-	print("Searching area ",area,"x",area)	#Fourth Quadrant
-	for r in range(area,2*area):
-		for c in range(area,2*area):
-			if (grid[r][c] == target):
-				print('Target is already in area')
-			else:
-				print(' - Not here')
+print("\n\n\n")
+for lol in range(3):
+	for target in range(1,5):
+		targetInQuadrant=False
+		placeCount=0
+		print("\n\n------------------------------\n\nLooking for", target)
+		print("Searching first quadrant ",area,"x",area)	#First Quadrant
+		for r in range(area):
+			for c in range(area):
+				print("target:",target)
+				if (grid[r][c] == target):
+					print("\nTarget is already in area")
+					targetInQuadrant=True
+				elif (grid[r][c] != 0):
+					print("\nLocation Taken, not placeable")
+				else:
+					print("\n",target," not here")
+					print("row num= ",r)
+					print("column num= ",c)
+					row=(grid[r])
+					print("\n","row =",row)
+					column=list(zip(*grid))
+					print("column: ",column[c],"\n")
+					print("targetin Quadrant?",targetInQuadrant)
+					if targetInQuadrant == True:
+						print("Target is already in quadrant, not placeable")
+					elif target in row or target in column[c]:
+						print("Target in row or column, not placeable")
+						placeable=False
+						placeCount=placeCount
+					else:
+						print("Target not in row or column, placeable")
+						placeable=True
+						placeCount = placeCount + 1
+						placeRow = r
+						placeColumn = c
+						print("placerow, placecolum:",placeRow, placeColumn)
+		if targetInQuadrant==True:
+			placeCount=0
+			for row in grid:
+				print(row)
+		if placeCount == 1:
+			print("Only one placeable location")
+			grid[placeRow][placeColumn] = target
+			print("PLACED\n\n\n\n")
+			for row in grid:
+				print(row)
+	##################################################################
+		targetInQuadrant=False
+		placeCount=0
+		print("\n\n------------------------------\n\nLooking for", target)
+		print("Searching second quadrant ",area,"x",area)
+		for r in range(area):
+			for c in range(area,2*area):
+				print("target:",target)
+				if (grid[r][c] == target):
+					print("\nTarget is already in area")
+					targetInQuadrant=True
+				elif (grid[r][c] != 0):
+					print("\nLocation Taken, not placeable")
+				else:
+					print("\n",target," not here")
+					print("row num= ",r)
+					print("column num= ",c)
+					row=(grid[r])
+					print("\n","row:",row)
+					column=list(zip(*grid))
+					print("colum:",column[c],"\n")
+					print("targetinquad?",targetInQuadrant)
+					if targetInQuadrant == True:
+						print("Target is already in quadrant, not placeable")
+					elif target in row or target in column[c]:
+						print("Target in row or column, not placeable")
+						placeable=False
+						placeCount=placeCount
+					else:
+						print("Target not in row or column, placeable")
+						placeable=True
+						placeCount = placeCount + 1
+						placeRow = r
+						placeColumn = c
+						print("placerowplacecolum:",placeRow, placeColumn)
+		if targetInQuadrant==True:
+			placeCount=0
+			for row in grid:
+				print(row)
+		if placeCount == 1:
+			print("Only one placeable location")
+			grid[placeRow][placeColumn] = target
+			print("PLACED\n\n\n\n")
+			for row in grid:
+				print(row)
+	###################################################################################
+		targetInQuadrant=False
+		placeCount=0
+		print("\n\n------------------------------\n\nLooking for", target)
+		print("Searching third quadrant ",area,"x",area)
+		for r in range(area,2*area):
+			for c in range(area):
+				print("target:",target)
+				if (grid[r][c] == target):
+					print("\nTarget is already in area")
+					targetInQuadrant=True
+				elif (grid[r][c] != 0):
+					print("\nLocation Taken, not placeable")
+				else:
+					print("\n",target," not here")
+					print("row num= ",r)
+					print("column num= ",c)
+					row=(grid[r])
+					print("\n","row:",row)
+					column=list(zip(*grid))
+					print("colum:",column[c],"\n")
+					print("targetinquad?",targetInQuadrant)
+					if targetInQuadrant == True:
+						print("Target is already in quadrant, not placeable")
+					elif target in row or target in column[c]:
+						print("Target in row or column, not placeable")
+						placeable=False
+						placeCount=placeCount
+					else:
+						print("Target not in row or column, placeable")
+						placeable=True
+						placeCount = placeCount + 1
+						placeRow = r
+						placeColumn = c
+						print("placerowplacecolum:",placeRow, placeColumn)
+		if targetInQuadrant==True:
+			placeCount=0
+			for row in grid:
+				print(row)
+		if placeCount == 1:
+			print("Only one placeable location")
+			grid[placeRow][placeColumn] = target
+			print("PLACED\n\n\n\n")
+			for row in grid:
+				print(row)
+	############################################################################
+		targetInQuadrant=False
+		placeCount=0
+		print("\n\n------------------------------\n\nLooking for", target)
+		print("Searching fourth quadrant ",area,"x",area)
+		for r in range(area,2*area):
+			for c in range(area,2*area):
+				print("target:",target)
+				if (grid[r][c] == target):
+					print("\nTarget is already in area")
+					targetInQuadrant=True
+				elif (grid[r][c] != 0):
+					print("\nLocation Taken, not placeable")
+				else:
+					print("\n",target," not here")
+					print("row num= ",r)
+					print("column num= ",c)
+					row=(grid[r])
+					print("\n","row:",row)
+					column=list(zip(*grid))
+					print("colum:",column[c],"\n")
+					print("targetinquad?",targetInQuadrant)
+					if targetInQuadrant == True:
+						print("Target is already in quadrant, not placeable")
+					elif target in row or target in column[c]:
+						print("Target in row or column, not placeable")
+						placeable=False
+						placeCount=placeCount
+					else:
+						print("Target not in row or column, placeable")
+						placeable=True
+						placeCount = placeCount + 1
+						placeRow = r
+						placeColumn = c
+						print("placerowplacecolum:",placeRow, placeColumn)
+		if targetInQuadrant==True:
+			placeCount=0
+			for row in grid:
+				print(row)
+		if placeCount == 1:
+			print("Only one placeable location")
+			grid[placeRow][placeColumn] = target
+			print("PLACED\n\n\n\n")
+			for row in grid:
+				print(row)
